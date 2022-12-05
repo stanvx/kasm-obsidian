e1830d75f98dbaef5db9b62d11c327e8c05f21f1
vorbildunternehmer <jj@vorbildunternehmer.de>
2022-12-05 23:03:04 +0100

    first commit

---
+++ src/ubuntu/install/obsidian/install_obsidian.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+OBSIDIAN_VERSION=1.0.3
+apt-get update
+apt install xdg-utils -y
+
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v$(echo $OBSIDIAN_VERSION)/obsidian_$(echo $OBSIDIAN_VERSION)_amd64.deb
+dpkg -i obsidian_$(echo $OBSIDIAN_VERSION)_amd64.deb
+rm -rf obsidian_$(echo $OBSIDIAN_VERSION)_amd64.deb
+
+
+rm -rf \
+  /var/lib/apt/lists/* \
+  /var/tmp/*
+
+# Default settings and desktop icon
+cp /usr/share/applications/obsidian.desktop $HOME/Desktop/obsidian.desktop
+chmod +x $HOME/Desktop/obsidian.desktop
